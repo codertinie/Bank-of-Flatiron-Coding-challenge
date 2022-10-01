f47c00a27c4eefbea2a7ff8b60962db2156aedbd
codertinie <martin.munyao@moringaschool.com>
2022-10-01 15:06:29 +0300

    Rendered the data in the table

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+//store fetched data in a state
+const [transactions, setTransactions] = useState([]) //starting with an empty array
+// use useEffect hook
+useEffect(() => {
+  //fetch data
+  fetch("http://localhost:8001/transactions")
+  .then((resp) => resp.json())
+  .then(transc => {
+    setTransactions(transc)
+  })
+    // console.log(transac)) // to confirm the fetch works
+},[]) // pass dependency array
+
+
   return (
     <div>
       <Search />
       <AddTransactionForm />
-      <TransactionsList />
+      <TransactionsList transactions={transactions}/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,6 @@ import React, { useEffect, useState } from "react";
 import AccountContainer from "./AccountContainer";
 
 function App() {
-  //store fetched data in a state
-  const [transactions, setTransactions] = useState([]) //starting with an empty array
-  // use useEffect hook
-  useEffect(() => {
-    //fetch data
-    fetch("http://localhost:3000/transactions")
-    .then(resp => resp.json())
-    .then(transac => setTransactions(transac))
-      // console.log(transac)) // to confirm the fetch works
-  },[]) // pass dependency array
-
-  console.log(transactions)
-
 
   return (
     <div className="ui raised segment">
